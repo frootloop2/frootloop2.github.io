5325805caab9543b93179e5cb0e5e193a4a1f6a1
frootloop2 <frankborden2@gmail.com>
2015-08-15 20:40:24 -0500

    fix render fail from github clone

--- render.js
+++ render.js
@@ -1,5 +1,26 @@
 window.Render = (function() {
-	function renderEntity(entity, view, camera, canvas, context) {
+	var canvas,
+		context;
+
+	canvas = document.createElement("canvas");
+	canvas.width = Math.min(window.innerWidth / 16, window.innerHeight / 9) * 16;
+	canvas.height = Math.min(window.innerWidth / 16, window.innerHeight / 9) * 9;
+	canvas.style.position = "absolute";
+	canvas.style.top = 0;
+	canvas.style.bottom = 0;
+	canvas.style.left = 0;
+	canvas.style.right = 0;
+	canvas.style.margin = "auto";
+	canvas.style.backgroundColor = "#FFFFFF";
+	document.body.appendChild(canvas);
+	window.onresize = function() {
+		canvas.width = Math.min(window.innerWidth / 16, window.innerHeight / 9) * 16;
+		canvas.height = Math.min(window.innerWidth / 16, window.innerHeight / 9) * 9;
+	};
+
+	context = canvas.getContext("2d");
+
+	function renderEntity(entity, view, camera) {
 		var widthRatio = canvas.width / view.width,
 			heightRatio = canvas.height / view.height;
 		context.fillStyle = entity.color;
@@ -10,19 +31,19 @@ window.Render = (function() {
 						 entity.color // color
 		);
 	};
+
 	return {
-		runSystem: function(model, canvas) {
-			var context = canvas.getContext("2d");
+		runSystem: function(model) {
 			context.clearRect(0, 0, canvas.width, canvas.height);
 			model.getEntities().forEach(function(entity) {
 				var clonedEntity;
-				renderEntity(entity, model.getView(), model.getCamera(), canvas, context);
+				renderEntity(entity, model.getView(), model.getCamera());
 				if(entity.wraps === true) {
 					clonedEntity = Entity.clone(entity);
 					clonedEntity.x += model.getView().width;
-					renderEntity(clonedEntity, model.getView(), model.getCamera(), canvas, context);
+					renderEntity(clonedEntity, model.getView(), model.getCamera());
 					clonedEntity.x -= 2 * model.getView().width;
-					renderEntity(clonedEntity, model.getView(), model.getCamera(), canvas, context);
+					renderEntity(clonedEntity, model.getView(), model.getCamera());
 				}
 			});
 		}
